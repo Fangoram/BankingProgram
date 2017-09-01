6cf89f96e0f6f20c7aaf83521637d97053717fd3
Zachary VanRossem <zacharyvanrossem@gmail.com>
2017-09-01 11:39:52 -0400

    All Files

---
+++ Banking_Program/Banking_Program/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [account_Name] NVARCHAR(50) NULL, 
+    [email_address] NVARCHAR(50) NULL, 
+    [checking_account] MONEY NOT NULL DEFAULT 0, 
+    [savings_account] MONEY NOT NULL DEFAULT 0
+)

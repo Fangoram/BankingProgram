2201f03867a28eaaed9b7a0b7aee6caeed871e4b
Zachary VanRossem <zacharyvanrossem@gmail.com>
2017-09-01 11:21:16 -0400

    Code
    The Header and Cpp files for BakingProgram.

---
+++ Banking_Program/Banking_Program/MenuNav.h
@@ -0,0 +1,14 @@
+#ifndef _MENUNAV_H_
+#define _MENUNAV_H_
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include "AccountData.h"
+
+void menuNav();
+void createAccount();
+void viewAllAccounts();
+void searchAccount();
+
+#endif
